2e7554a33e419b2e40bbe1e6f60ee0112a6ad8fb
Alexqq11 <alt.alexqq11@gmail.com>
2016-12-12 14:47:17 +0500

    adde structure class for fsin info ldir and dir tables in fat fs

--- Core.py
+++ Core.py
@@ -1,94 +1,109 @@
 import mmap
 import os
 import struct
+
 """"reserved region class """
 """ bs - boot sector"""
 """"bpb bios parameter block """
 
-class core:
+
+class Core:
     def __init__(self):
         self.image_reader = None
         self.fat_bot_sector = None
+
     def _init_image(self, path):
         self.image_reader = ImageReader(path)
+
     def _init_fat_boot_sector(self):
         self.fat_bot_sector = FatBootSector(self.image_reader)
+
     def init(self, path):
         self._init_image(path)
         self._init_fat_boot_sector()
+
     def close_reader(self):
         self.image_reader.close_reader()
+
     pass
 
 
 class FatBootSector:
-    def __init__(self,image_reader):
+    def __init__(self, image_reader):
         self.reader = image_reader
-        self.bs_jmp_boot = None #0 3
-        self.bs_oem_name = None #3 8
-        self.bpb_bytes_per_sector = None #11 2
-        self.bpb_sectors_per_cluster = None #13 1
-        self.bpb_reserved_region_sectors_count = None #14 2
-        self.bpb_number_fats = None # 16 1
-        self.bpb_root_entry_count = None #17 2 for fat32 it zero
-        self.bpb_total_sectors_16 = None #19 2 old sixteen bits field in fat 32 must be zero
-        self.bpb_media = None # 21 1 stand
-        self.bpb_fat_size_16 = None # 22 2 amount fat sectors for one fat12/16 table in fat32 zero watch to fat 32
-        self.bpb_sectors_per_track = None # 24 2 for interrupt 13 and accses to disks with geometry #old tech
-        self.bpb_number_heads = None # 26 2 ammount of disk heads
-        self.bpb_hidden_sectors = None # 28 4
-        self.bpb_total_sectors_32 = None # 32 4 new 32 bit field sm old 16 bit field
+        self.bs_jmp_boot = None  # 0 3
+        self.bs_oem_name = None  # 3 8
+        self.bpb_bytes_per_sector = None  # 11 2
+        self.bpb_sectors_per_cluster = None  # 13 1
+        self.bpb_reserved_region_sectors_count = None  # 14 2
+        self.bpb_number_fats = None  # 16 1
+        self.bpb_root_entry_count = None  # 17 2 for fat32 it zero
+        self.bpb_total_sectors_16 = None  # 19 2 old sixteen bits field in fat 32 must be zero
+        self.bpb_media = None  # 21 1 stand
+        self.bpb_fat_size_16 = None  # 22 2 amount fat sectors for one fat12/16 table in fat32 zero watch to fat 32
+        self.bpb_sectors_per_track = None  # 24 2 for interrupt 13 and accses to disks with geometry #old tech
+        self.bpb_number_heads = None  # 26 2 ammount of disk heads
+        self.bpb_hidden_sectors = None  # 28 4
+        self.bpb_total_sectors_32 = None  # 32 4 new 32 bit field sm old 16 bit field
         # there was can been fat12/16 fields but we starting write fat 32 fields
         self.bpb_fat_size_32 = None  # 36 4 amoun of sectors one fat
-        self.bpb_ext_flags = None #40 2
-        self.file_system_version = None #42 2
-        self.bpb_root_cluster = None# 44 4
-        self.bpb_file_system_information = None #48 2
-        self.bpb_backup_boot_sector = None # 50 2
-        self.bpb_reserved = None # 52 12
-        self.bs_driver_number = None # 64 1
-        self.bs_reserved1 = None # 65 1
-        self.bs_boot_signature = None# 66 1
-        self.bs_volume_id = None# 67 4
-        self.bs_volume_label = None # 71 11
-        self.bs_file_system_type = None #82 8
+        self.bpb_ext_flags = None  # 40 2
+        self.file_system_version = None  # 42 2
+        self.bpb_root_cluster = None  # 44 4
+        self.bpb_file_system_information = None  # 48 2
+        self.bpb_backup_boot_sector = None  # 50 2
+        self.bpb_reserved = None  # 52 12
+        self.bs_driver_number = None  # 64 1
+        self.bs_reserved1 = None  # 65 1
+        self.bs_boot_signature = None  # 66 1
+        self.bs_volume_id = None  # 67 4
+        self.bs_volume_label = None  # 71 11
+        self.bs_file_system_type = None  # 82 8
         self._read_fat_boot_sector()
+
     def get_fat_offset(self):
-        pass
+        return self.bpb_bytes_per_sector * self.bpb_reserved_region_sectors_count
+
+    def get_root_dir_offset(self):
+        return self.get_fat_offset() + self.bpb_number_fats * self.bpb_fat_size_32 * self.bpb_bytes_per_sector
+
     def _read_fat_boot_sector(self):
-        self.bs_jmp_boot = self.reader.get_data(0,3) # 0 3
-        self.bs_oem_name = self.reader.get_data(3,8) # 3 8
-        self.bpb_bytes_per_sector = self.reader.get_data(11,2,True) # 11 2
-        self.bpb_sectors_per_cluster = self.reader.get_data(12,1,True)  # 13 1
-        self.bpb_reserved_region_sectors_count = self.reader.get_data(14,2,True) # 14 2
-        self.bpb_number_fats = self.reader.get_data(16,1,True)# 16 1
-        self.bpb_root_entry_count = self.reader.get_data(17,2,True) # 17 2 for fat32 it zero
-        self.bpb_total_sectors_16 = self.reader.get_data(19,2,True) # 19 2 old sixteen bits field in fat 32 must be zero
-        self.bpb_media = self.reader.get_data(21,1)  # 21 1 stand
-        self.bpb_fat_size_16 = self.reader.get_data(22,2,True)  # 22 2 amount fat sectors for one fat12/16 table in fat32 zero watch to fat 32
-        self.bpb_sectors_per_track = self.reader.get_data(24,2,True) # 24 2 for interrupt 13 and accses to disks with geometry #old tech
-        self.bpb_number_heads = self.reader.get_data(26,2,True) # 26 2 ammount of disk heads
-        self.bpb_hidden_sectors = self.reader.get_data(28,4,True)  # 28 4
-        self.bpb_total_sectors_32 = self.reader.get_data(32,4,True) # 32 4 new 32 bit field sm old 16 bit field
+        self.bs_jmp_boot = self.reader.get_data(0, 3)  # 0 3
+        self.bs_oem_name = self.reader.get_data(3, 8)  # 3 8
+        self.bpb_bytes_per_sector = self.reader.get_data(11, 2, True)  # 11 2
+        self.bpb_sectors_per_cluster = self.reader.get_data(13, 1, True)  # 13 1
+        self.bpb_reserved_region_sectors_count = self.reader.get_data(14, 2, True)  # 14 2
+        self.bpb_number_fats = self.reader.get_data(16, 1, True)  # 16 1
+        self.bpb_root_entry_count = self.reader.get_data(17, 2, True)  # 17 2 for fat32 it zero
+        self.bpb_total_sectors_16 = self.reader.get_data(19, 2,
+                                                         True)  # 19 2 old sixteen bits field in fat 32 must be zero
+        self.bpb_media = self.reader.get_data(21, 1)  # 21 1 stand
+        self.bpb_fat_size_16 = self.reader.get_data(22, 2,
+                                                    True)  # 22 2 amount fat sectors for one fat12/16 table in fat32 zero watch to fat 32
+        self.bpb_sectors_per_track = self.reader.get_data(24, 2,
+                                                          True)  # 24 2 for interrupt 13 and accses to disks with geometry #old tech
+        self.bpb_number_heads = self.reader.get_data(26, 2, True)  # 26 2 ammount of disk heads
+        self.bpb_hidden_sectors = self.reader.get_data(28, 4, True)  # 28 4
+        self.bpb_total_sectors_32 = self.reader.get_data(32, 4, True)  # 32 4 new 32 bit field sm old 16 bit field
         # there was can been fat12/16 fields but we starting write fat 32 fields
-        self.bpb_fat_size_32 = self.reader.get_data(36,4,True) # 36 4 amoun of sectors one fat
-        self.bpb_ext_flags = self.reader.get_data(40,2) # 40 2
-        self.file_system_version = self.reader.get_data(42,2)  # 42 2
-        self.bpb_root_cluster = self.reader.get_data(44,4,True) # 44 4
-        self.bpb_file_system_information = self.reader.get_data(48,2,True)  # 48 2 кол-во свободных кластеров на диске, чтобы утилитам легче жить
-        self.bpb_backup_boot_sector = self.reader.get_data(50,2,True) # 50 2
-        self.bpb_reserved = self.reader.get_data(52,12)  # 52 12
+        self.bpb_fat_size_32 = self.reader.get_data(36, 4, True)  # 36 4 amoun of sectors one fat
+        self.bpb_ext_flags = self.reader.get_data(40, 2)  # 40 2
+        self.file_system_version = self.reader.get_data(42, 2)  # 42 2
+        self.bpb_root_cluster = self.reader.get_data(44, 4, True)  # 44 4
+        self.bpb_file_system_information = self.reader.get_data(48, 2,
+                                                                True)  # 48 2 кол-во свободных кластеров на диске, чтобы утилитам легче жить
+        self.bpb_backup_boot_sector = self.reader.get_data(50, 2, True)  # 50 2
+        self.bpb_reserved = self.reader.get_data(52, 12)  # 52 12
         self.bs_driver_number = self.reader.get_data(64, 1)  # 64 1
-        self.bs_reserved1 = self.reader.get_data(65,1)  # 65 1
+        self.bs_reserved1 = self.reader.get_data(65, 1)  # 65 1
         self.bs_boot_signature = self.reader.get_data(66, 1)  # 66 1
-        self.bs_volume_id = self.reader.get_data(67,4)  # 67 4
-        self.bs_volume_label = self.reader.get_data(71,11)  # 71 11
-        self.bs_file_system_type = self.reader.get_data(82,8)  # 82 8
+        self.bs_volume_id = self.reader.get_data(67, 4)  # 67 4
+        self.bs_volume_label = self.reader.get_data(71, 11)  # 71 11
+        self.bs_file_system_type = self.reader.get_data(82, 8)  # 82 8
         # end boot sector signature 0xaa55 have been lost
-    def count_fat_table_offset(self,lol):
+
+    def count_fat_table_offset(self, lol):
         pass
-
-
 
 
 class ImageReader:
@@ -102,29 +117,64 @@ class ImageReader:
             self.file_stream = f
             self.image = mmap.mmap(f.fileno(), 0)
 
-    def _get_parse_mod(self,size):
+    def _get_parse_mod(self, size):
         mod_parameter = ''
         if (size == 1):
             mod_parameter = '<B'
-        elif(size == 2):
+        elif (size == 2):
             mod_parameter = '<H'
-        elif(size == 4):
+        elif (size == 4):
             mod_parameter = '<I'
         return mod_parameter
 
     def get_data(self, offset, size, convert_integer=False):
         self.image.seek(offset)
         buffer = self.image.read(size)
-        if(convert_integer):
-            buffer = struct.unpack(self._get_parse_mod(size),buffer)[0]
+        if (convert_integer):
+            buffer = struct.unpack(self._get_parse_mod(size), buffer)[0]
         return buffer
 
     def close_reader(self):
         self.image.close()
         self.file_stream.close()
+class FSInfo:
+    def __init__(self):
+        self.fsi_lead_signature = None # 0 4
+        self.fsi_reserved1 = None # 4 480
+        self.fsi_structure_signature = None # 484 4
+        self.fsi_free_count = None # 488 4
+        self.fsi_next_free = None #492 4
+        self.fsi_reserved2 = None #496 12
+        self.fsi_trail_signature = None # 508 4
 
-c = core()
+class DirEntryShortFat32:
+    def __init__(self):
+        self.dir_name = None # 0 11
+        self.dir_attributes = None # 11 1
+        self.dir_nt_reserved = None #12 1
+        self.dir_create_time_tenth = None #13 1
+        self.dir_create_time = None #14 2
+        self.dir_create_date= None # 16 2
+        self.dir_last_access_date = None # 18 2
+        self.dir_first_cluster_high = None #20 2 // старшее слово номера первого кластера
+        self.dir_write_time = None # 22 2 время последней записи , создание тоже запись
+        self.dir_write_date = None # 24 2 дата последней записи,создание файла тоже запись
+        self.dir_first_cluster_low = None # 26 2 младшее слово первого кластера (склей их и будет тебе счастье
+        self.dir_file_size = None # 28 4
+class DirEntryLongFat32:
+    def __init__(self):
+        self.ldir_order = None # 0 1
+        self.ldir_name1 = None # 1 10
+        self.ldir_attribute = None # 11 1
+        self.ldir_type = None # 12 1
+        self.ldir_check_sum = None # 13 1
+        self.ldir_name2 = None # 14 12
+        self.ldir_first_cluster_low = None # 26 2 must be zero
+        self.ldir_name3 = None # 28 4
+c = Core()
 c.init("..\.\dump (1).iso")
 print(c.fat_bot_sector.__dict__)
+print(c.fat_bot_sector.get_fat_offset())
+print(c.fat_bot_sector.get_root_dir_offset())
 
 c.close_reader()
