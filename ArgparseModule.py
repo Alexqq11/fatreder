0f3802f3287bd157e5e99c89e479fb0e4b4b6efe
Alexqq11 <alt.alexqq11@gmail.com>
2017-02-10 03:10:17 +0500

    deleted old code base and dirty code from files in coments , added pytonic comand parser

---
+++ ArgparseModule.py
@@ -0,0 +1,93 @@
+import argparse
+import shlex
+
+
+class ArgsParser:
+    def __init__(self):
+        self.parser = None
+        self.subparsers = None
+        self.init_parsers()
+
+    def parse(self, args_list):
+        return self.parser.parse_args(args_list), args_list[0]
+
+    def get_args_list(self, string):
+        return shlex.split(string)
+
+    def init_parsers(self):
+        # global parser
+        parser_program = argparse.ArgumentParser(prog="fatreader",
+                                                 description='watch an extract file and directories in fat32 images')
+        parser_program.add_argument('-n', "--no-scan", dest='noscan', action='store_true',
+                                    help='do not check the image for errors')
+        parser_program.add_argument('-k', "--keep-alive", dest='keep_alive', action='store_true',
+                                    help='do not exit from util after get sys.args')
+        parser_program.add_argument('-l', "--load",  dest='load', action='store', help=' load working image from path')
+        subparsers = parser_program.add_subparsers(help='sub-command help')
+
+        parser_load = subparsers.add_parser("load" ,help="load -h , --help", description="load open image")
+        parser_load.add_argument(dest='path', metavar='path', nargs=1)
+        parser_load.add_argument('-n', "--no-scan", dest='noscan', action='store_true',
+                                    help='do not check the image for errors')
+        # ls
+        parser_ls = subparsers.add_parser("ls", help="ls -h, --help",
+                                          description='ls is a command to print list files in directory in fat reader util')
+        parser_ls.add_argument(dest='path', metavar='path', nargs='?')  # * - all ? zero or one
+        parser_ls.add_argument('-l', '--long', dest='long', action='store_true',
+                               help='print file information in human readable format')
+        parser_ls.add_argument('-a', '--all', dest='all', action='store_true',
+                               help='print file information for all files in directory')
+        parser_ls.add_argument('-r', '--recursive', dest='recursive', action='store_true',
+                               help='print file information for files in directory and subdirectories')
+
+        # cp
+        parser_cp = subparsers.add_parser("cp", help="cp -h, --help",
+                                          description='cp is a command to copy files or directories from/on/in image')
+        parser_cp.add_argument(dest='path', metavar='path', nargs=2)  # * - all ? zero or one
+        parser_cp.add_argument('-e', '--external', dest='external', action='store_true',
+                               help='copy file or directory from os to image')
+        parser_cp.add_argument('-i', '--internal', dest='internal', action='store_true',
+                               help='copy file or directory from image to os')
+
+        # cd
+        parser_cd = subparsers.add_parser("cd", help="cd -h, --help",
+                                          description='cd is a command to change dir in fat32 image')
+        parser_cd.add_argument(dest='path', metavar='path', nargs='?')  # * - all ? zero or one | number
+
+        # md
+        parser_md = subparsers.add_parser("md", help="md -h, --help",
+                                          description='md is a command to make directory(ies) in image')
+        parser_md.add_argument(dest='path', metavar='path', nargs=1)  # * - all ? zero or one | number
+
+        # pwd
+        parser_pwd = subparsers.add_parser("pwd", help="pwd -h, --help",
+                                           description='pwd is a command to calculate ful path to working directory in image')
+
+        # move
+        parser_move = subparsers.add_parser("move", help="move -h, --help",
+                                            description='move is a command to move files or directories in image')
+        parser_move.add_argument(dest='path', metavar='path', nargs=2)  # * - all ? zero or one
+
+        # cat
+        parser_cat = subparsers.add_parser("cat", help="cat -h, --help",
+                                           description='cat is a command to print file content')
+        parser_cat.add_argument(dest='path', metavar='path', nargs=1)  # * - all ? zero or one
+        parser_cat.add_argument('-b', '--byte', dest='byte', action='store_true', help='get data in hex stream')
+        parser_cat.add_argument('-t', '--all', dest='all', action='store_true', help='get data in text stream')
+        parser_cat.add_argument('-e', '--encoding', dest='encoding', action='store', help=' set data encoding')
+
+        # rm
+        parser_rm = subparsers.add_parser("rm", help="rm -h, --help",
+                                          description="rm is a command to delete file in image")
+        parser_rm.add_argument(dest='path', metavar='path', nargs=1)
+        parser_rm.add_argument('-c', '--clear', dest="clear", action="store_true",
+                               help='erase data on cluster by zeros')
+
+        # rmdir
+        parser_rmdir = subparsers.add_parser("rmdir", help="rmdir -h, --help",
+                                             description="rmdir is a command to delete directories in image")
+        parser_rmdir.add_argument(dest='path', metavar='path', nargs=1)
+        parser_rmdir.add_argument('-c', '--clear', dest="clear", action="store_true",
+                                  help='erase data on cluster by zeros')
+        self.parser = parser_program
+        self.subparsers = subparsers
--- Core.py
+++ Core.py
@@ -3,12 +3,7 @@ import FileSystemUtils as Fsw
 import ImageWorker as Image
 import ReservedRegionReader as Rrr
 from  FatReaderExceptions import *
-from  CUICommandParser import *
-""""reserved region class """
-""" bs - boot sector"""
-""""bpb bios parameter block """
-
-
+import ArgparseModule
 class Core:
     def __init__(self):
         self.image_reader = None
@@ -40,115 +35,14 @@ class Core:
         self.image_reader.close_reader()
 
     pass
-
-
 if __name__ == "__main__":
     c = Core()
     c.init("..\.\dump (1).iso")
 
-    def initCommands(c : Core):
-        commands_list = []
-        cd = Command("cd", c.file_system_utils.change_directory,dict())
-        commands_list.append(cd)
-        ls = Command("ls",c.file_system_utils.get_working_directory_information,dict())
-        ls.add_function_flags(("h","ls"),{"hidden": True})
-        ls.add_function_flags(("a", "ls"),{"datetime": True, "attributes": True,"size": True})
-        ls.add_function_flags(("a", "h", "ls"), {"datetime": True, "attributes": True, "size": True, "hidden": True})
-        commands_list.append(ls)
-        rm = Command("rm",c.file_system_utils.remove_file,{"recoverable":False})
-        rm.add_function_flags(tuple("c"), {"recoverable" : False, "clear": True})
-        rm.add_function_flags(tuple("r"), {"recoverable" : True, "clear": False})
-        commands_list.append(rm)
-        cp = Command("cp", None, dict())
-        cp.add_function_flags(tuple("f"), dict(),c.file_system_utils.copy_on_image)
-        cp.add_function_flags(tuple("d"), dict(),c.file_system_utils.copy_directory)
-        commands_list.append(cp)
-        mkdir = Command("mkdir", c.file_system_utils.new_directory,dict())
-        commands_list.append(mkdir)
-        info = Command("info", c.file_system_utils.get_file_information,dict())
-        commands_list.append(info)
-        transfer = Command("transfer",c.file_system_utils.transfer,dict())
-        commands_list.append(transfer)
-        rename = Command("rename", c.file_system_utils.rename, dict())
-        commands_list.append(rename)
-        return  commands_list
-
-    command_executor = CommandExecutor(initCommands(c))
     while (True):
         try:
             command_prompt = input("]>")
-            command_executor.execute_command(command_prompt)
         except FatReaderException:
             pass
 
-
-            """""""""
-            args = [x for x in inp.split()]
-            command =CommandParser(inp)
-            command.command = args[0]
-            if (args[0] == 'cd'):
-                c.file_system_utils.change_directory(command.path[0])
-            elif (command.command == 'ls'):
-                info = c.file_system_utils.get_working_directory_information()
-                for entry in info:
-                    print(entry)
-            elif (command.command == 'pwd'):
-                print(c.file_system_utils.calculate_directory_path())
-            elif (command.command == 'info'):
-                print(c.file_system_utils.get_file_information(args[1]))
-            elif (command.command == 'help'):
-                print("cd , ls . pwd, info ,cp, mkdir, rn, ts, exit, help")
-            elif (command.command == 'exit'):
-                break
-            elif (command.command == 'mkdir'):
-                c.file_system_utils.new_directory(join_name(args))
-            elif (command.command == 'cp'):
-                c.file_system_utils.copy_on_image(args[1], args[2])
-            elif (command.command == 'cp-d'):
-                c.file_system_utils.copy_directory(args[1], args[2])
-            elif (command.command == 'rn'):
-                c.file_system_utils.rename(args[1], args[2])
-            elif (command.command == 'ts'):
-                c.file_system_utils.transfer(args[1], args[2])
-            elif (command.command == 'rm-r'):
-                c.file_system_utils.remove_file(join_name(args))
-            elif (command.command == 'rm-a'):
-                c.file_system_utils.remove_file(join_name(args), recoverable=False)
-            elif (command.command == 'rm-c'):
-                c.file_system_utils.remove_file(join_name(args), clean=True)
-            elif (command.command == 'cat'):
-                data = c.file_system_utils.cat_data(args[1])
-                i = iter(data)
-                encoding = 'utf-8'
-                if len(args) == 3:
-                    encoding = args[2]
-                print(next(i).decode(encoding))
-            else:
-                print('command not found')
-       """""""""
-    """""""""
-    c.file_system_utils.change_directory('/архЭВм/Конспекты от Артура/')
-    c.file_system_utils.change_directory('../')
-    info = c.file_system_utils.get_working_directory_information()
-    for entry in info:
-        print(entry)
-    print(c.file_system_utils.calculate_directory_path())
-    c.file_system_utils.change_directory('../')
-    info = c.file_system_utils.get_working_directory_information()
-    for entry in info:
-        print(entry)
-    print(c.file_system_utils.calculate_directory_path())
-    print(c.file_system_utils.get_file_information("архЭВМ"))
-    """
-    # dir  = c.dir_parser.nio_parse_directory(c.fat_bot_sector.get_root_dir_offset())
-    # for file in dir.files:
-    # print(file)
-    #   dir.files[file].set_user_representation()
-    #  print(dir.files[file].human_readable_view.to_string())
-    # print(dir.files)
-    """""""""
-    for x in c.dir_parser.File_entries:
-        x.set_user_representation()
-        print(x.human_readable_view.to_string())
-    """""
     c.close_reader()
